c3f3eeed23717682f13fec1c2221fe711cd3fc0d
WittyWhiscash <videoguy1998@hotmail.com>
2020-04-22 18:05:34 -0600

    Lowered lighting volume, added leaf drops (Addresses Issue #1)

--- src/main/java/mod/wittywhiscash/immersivelighting/events/LootTableHandler.java
+++ src/main/java/mod/wittywhiscash/immersivelighting/events/LootTableHandler.java
@@ -15,6 +15,13 @@ public class LootTableHandler {
     private static ResourceLocation fern_drops = new ResourceLocation("minecraft", "blocks/fern");
     private static ResourceLocation tallgrass_drops = new ResourceLocation("minecraft", "blocks/tall_grass");
 
+    private static ResourceLocation acacialeaves_drops = new ResourceLocation("minecraft", "blocks/acacia_leaves");
+    private static ResourceLocation birchleaves_drops = new ResourceLocation("minecraft", "blocks/birch_leaves");
+    private static ResourceLocation darkoakleaves_drops = new ResourceLocation("minecraft", "blocks/dark_oak_leaves");
+    private static ResourceLocation jungleleaves_drops = new ResourceLocation("minecraft", "blocks/jungle_leaves");
+    private static ResourceLocation oakleaves_drops = new ResourceLocation("minecraft", "blocks/oak_leaves");
+    private static ResourceLocation spruceleaves_drops = new ResourceLocation("minecraft", "blocks/spruce_leaves");
+
     @SubscribeEvent
     public static void lootTableLoad(LootTableLoadEvent event) {
         if (event.getName().equals(grass_drops) || event.getName().equals(fern_drops) || event.getName().equals(tallgrass_drops)) {
@@ -23,6 +30,14 @@ public class LootTableHandler {
                     .name("il_grass_drops").build());
             if (Config.DEBUG_SHOWDEBUG.get()) {
                 ImmersiveLighting.LOGGER.debug("Added Grass Drops to Grass Drop Loot Tables.");
+            }
+        }
+        if (event.getName().equals(acacialeaves_drops) || event.getName().equals(birchleaves_drops) || event.getName().equals(darkoakleaves_drops) || event.getName().equals(jungleleaves_drops) || event.getName().equals(oakleaves_drops) || event.getName().equals(spruceleaves_drops)) {
+            event.getTable().addPool(LootPool.builder()
+                    .addEntry(TableLootEntry.builder(new ResourceLocation(ImmersiveLighting.MODID, "blocks/leaves_drops")))
+                    .name("il_leaves_drops").build());
+            if (Config.DEBUG_SHOWDEBUG.get()) {
+                ImmersiveLighting.LOGGER.debug("Added Leaves Drops to Leaves Drop Loot Tables.");
             }
         }
     }
